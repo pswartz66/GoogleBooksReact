cc527775318906ac0550285eb965de645c83311b
Phil Swartz <pswartz.sju@gmail.com>
2020-01-02 20:29:49 -0500

    added setup files and folders

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './App.css';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/utils/API.js
@@ -0,0 +1,5 @@
+
+import dotenv from 'dotenv';
+// import axios from 'axios';
+const BookSearchURL = 'https://www.googleapis.com/books/v1/volumes?q='
+const key = dotenv.key;
