1d73c59665360e3d1649c45f8fc56d99fa60df6d
Phil Swartz <pswartz.sju@gmail.com>
2020-01-18 13:15:38 -0500

    installed boostrap

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 
 // Connect to Mongo
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI); //|| "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 
 // Start the Server
